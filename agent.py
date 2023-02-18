5d79ef5ba677473b664e9840cf66667bc97aa343
Jeongwoo <18241728+ryuqae@users.noreply.github.com>
2023-02-18 15:55:37 +0900

    modified:   .gitignore 	modified:   agent.py 	modified:   app.py

--- agent.py
+++ agent.py
@@ -5,31 +5,43 @@ load_dotenv()
 
 openai.api_key = os.getenv("APIKEY")
 
-# print(openai.Model.list())
 
 completion = openai.Completion()
 
 
 class Agent:
-    def __init__(self, prompt):
+    def __init__(
+        self,
+        prompt,
+        engine,
+        temperature,
+        max_tokens,
+        frequency_penalty,
+        presence_penalty,
+    ):
         self.prompt = prompt
+        self.engine = engine
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
 
     def respond(self, message):
         response = completion.create(
-            engine="text-davinci-003",
-            prompt=self.prompt + message,
-            temperature=0.9,
-            max_tokens=1000,
+            engine=self.engine,
+            prompt=self.prompt + message + "\n- AI: ",
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
             top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6,
-            stop=["\n", "AI: "],
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            stop=["- AI: "],
         )
         return response.choices[0].text
 
 
 if __name__ == "__main__":
-    prompt = "The following is a conversation with an AI assistant. The assistant is empathetic, helpful and very friendly.\n\n"
+    prompt = "The following is a conversation with an AI assistant. The assistant is empathetic, helpful and very friendly."
     agent = Agent(prompt)
     message = """Human: 오늘 친구랑 심하게 다퉜어. 친구는 아주 오래된 친구고 항상 붙어다녔는데 이렇게 싸우고 나니 마음이 좋지 않아. 이런 상황에서 내가 먼저 친구한테 연락하는게 좋을까?\n AI: """
 
--- app.py
+++ app.py
@@ -1,14 +1,36 @@
 from agent import Agent
 import streamlit as st
 
-prompt = "The following is a conversation with an AI assistant. The assistant is empathetic, helpful and very friendly.\n\n"
+prompt = "The following is a conversation with an AI assistant. The assistant is empathetic, helpful and very friendly. Please respond to following human feelings or experiences.\n\n- Human: "
 
-agent = Agent(prompt)
 
 st.title("AI Chatbot")
 
+st.write("잎클이: 안녕하세요. 잎클이입니다. 무엇이든 물어보세요.")
+
+with st.sidebar:
+    temperature = st.slider("Temperature", 0.0, 1.0, 0.9, 0.05)
+    max_tokens = st.slider("Max Tokens", 10, 1000, 200, 10)
+    frequency_penalty = st.slider("Frequency Penalty", 0.0, 1.0, 0.0, 0.05)
+    presence_penalty = st.slider("Presence Penalty", 0.0, 1.0, 0.6, 0.05)
+
+    engine = st.radio(
+        "Engine", ["text-davinci-003", "text-davinci-002", "text-davinci-001"]
+    )
+
+
+agent = Agent(
+    prompt=prompt,
+    engine=engine,
+    temperature=temperature,
+    max_tokens=max_tokens,
+    frequency_penalty=frequency_penalty,
+    presence_penalty=presence_penalty,
+)
+
 message = st.text_area("고민거리")
+st.write(agent.prompt + message)
 
 if st.button("Send"):
-    completion = agent.respond(message + "잎클이: ")
+    completion = agent.respond(message)
     st.write("잎클이: ", completion)
